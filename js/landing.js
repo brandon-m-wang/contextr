87076cc90253f0bb7e11c24ed4f87f3e0d5f6c52
brandon wang <brandonwang0503@berkeley.edu>
2020-12-24 02:16:32 -0600

    tf

--- js/landing.js
+++ js/landing.js
@@ -8,7 +8,7 @@ loginBtn.addEventListener('click', (e) => {
 	let parent = e.target.parentNode.parentNode;
 	Array.from(e.target.parentNode.parentNode.classList).find((element) => {
 		if(element !== "slide-up") {
-			parent.classList.add('slide-up')
+            
 		}else{
 			signupBtn.parentNode.classList.add('slide-up')
 			parent.classList.remove('slide-up')
@@ -20,7 +20,7 @@ signupBtn.addEventListener('click', (e) => {
 	let parent = e.target.parentNode;
 	Array.from(e.target.parentNode.classList).find((element) => {
 		if(element !== "slide-up") {
-			parent.classList.add('slide-up')
+            
 		}else{
 			loginBtn.parentNode.parentNode.classList.add('slide-up')
 			parent.classList.remove('slide-up')
