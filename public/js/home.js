dad6be8efd338d3212e49b47232fe02821037a61
brandon wang <70977266+brandon-m-wang@users.noreply.github.com>
2021-01-07 05:28:52 -0600

    im literally frontend god

--- public/js/home.js
+++ public/js/home.js
@@ -23,4 +23,24 @@ $(document).ready(function () {
             $('body').removeClass('modal-active');
         }
     });
+
+    $('#slide-left').click(function (){
+        $('.post-style-selections').css('left', '0')
+    })
+
+    $('#slide-right').click(function (){
+        let windowSize = $(window).width();
+        console.log(windowSize*0.336);
+        $('.post-style-selections').css('left', ((965-windowSize*0.336)*-1).toString() + 'px')
+    })
+
+    $('.prompt-name-image').click(function (){
+        $('.prompt-dropdown').css({'margin-top': '-85px', 'opacity': '1'});
+        //write cite function
+    })
+
+    $('#cancel').click(function (){
+        $('.prompt-dropdown').css({'margin-top': '-255px', 'opacity': '0'});
+    })
+
 });
