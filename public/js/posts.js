84f88a7b4a651b41b4c5a226b0f7e03b51f2e995
zanenasrallah <71055457+zanenasrallah@users.noreply.github.com>
2021-01-08 20:46:39 -0600

    changes

--- public/js/posts.js
+++ public/js/posts.js
@@ -31,3 +31,44 @@ function getPosts(){
         }
     });
 }
+
+function createID(){
+    var unique = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < 7; i++ ) {
+        unique += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    const now = String(Date.now());
+    const middlePos = Math.ceil(now.length / 2);
+    let output = ("" + (now.substr(0, middlePos)).concat(unique)).concat(now.substr(middlePos));
+    return output;
+}
+
+function createPost(){
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            var db = firebase.firestore();
+            var postID = createID();
+            var writer = firebase.auth().currentUser.uid;
+            var cited = "Vf9l4Unb5qdShTx77r2DC1KdMG43"; //Change this to whoever is being posted about
+            var quote = "testing quote"; //Change to actual quot
+            var theme = "happy"; //Change to theme
+
+            db.collection('posts').doc(postID).set({
+                poster: writer,
+                postee: cited,
+                theme: theme,
+                text: quote,
+                comments: [],
+                likes: []
+            })
+            db.collection('userspublic').doc(writer).update({
+                notifications: firebase.firestore.FieldValue.arrayUnion(postID) 
+            })
+            db.collection('users').doc(writer).update({
+                posts: firebase.firestore.FieldValue.arrayUnion(postID)                
+            })
+        }
+    });
+}
