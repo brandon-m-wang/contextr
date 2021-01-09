6f7d1270bae32605289902e4f40ff3ddb14e347b
brandon wang <70977266+brandon-m-wang@users.noreply.github.com>
2021-01-08 20:26:30 -0600

    populating homepage w friends and fetching correct citee

--- public/js/home.js
+++ public/js/home.js
@@ -2,6 +2,12 @@
     history.pushState({id: "profile"}, "contextr.io | Profile", "profile.html");
     window.location.href = "profile.html";
 }*/
+function htmlToElement(html) {
+    var template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
 
 $(document).ready(function () {
 
@@ -24,23 +30,67 @@ $(document).ready(function () {
         }
     });
 
-    $('#slide-left').click(function (){
-        $('.post-style-selections').css('left', '0')
-    })
+    firebase.auth().onAuthStateChanged(async function (user) {
+        if (user) {
+            var userID = firebase.auth().currentUser.uid;
+            console.log(userID);
+            await firebase.firestore().collection("users").doc(userID).get().then(async function (doc) {
+                for (let i = 0; i < doc.data().friends.length && i < 6; i++) {
+                    var friendID = await doc.data().friends[i]
+                    await firebase.firestore().collection("users").doc(friendID).get().then(async function (doc) {
+                        var name = '@' + await doc.data().name
+                        let htmlString = htmlToElement(`<div class="prompt-name-image" id='${'prompt' + (i+1).toString()}'>
+                        <p id="${name}">${name}</p>
+                    </div>`)
+                        document.getElementsByClassName('prompt-images')[0].appendChild(htmlString)
+                    })
+                    await firebase.storage().ref().child('users/' + friendID + '/profile').getDownloadURL().then(async function (result) {
+                        var imgUrl = await result
+                        let imgString = await htmlToElement(`<img src='${imgUrl}'>`)
+                        document.getElementById("prompt" + (i+1).toString()).prepend(imgString);
+                    })
+                }
+                for (let i = 0; i < doc.data().friends.length; i++){
+                    var friendID = await doc.data().friends[i]
+                    await firebase.firestore().collection("users").doc(friendID).get().then(async function (doc) {
+                        var name = '@' + await doc.data().name
+                        let htmlString2 = htmlToElement(`<div class="${'request' + ' ' + i.toString()}">
+                    <div class="request-info">
+                        <h6>${name}</h6>
+                    </div>
+                    <div class="request-actions">
+                        <a>Cite</a>
+                        <a>View</a>
+                    </div>
+                </div>`)
+                        document.getElementsByClassName('requests')[0].appendChild(htmlString2)
+                    })
+                    await firebase.storage().ref().child('users/' + friendID + '/profile').getDownloadURL().then(async function (result) {
+                        var imgUrl = await result
+                        let imgString = await htmlToElement(`<img src='${imgUrl}'>`)
+                        document.getElementsByClassName(i.toString())[0].prepend(imgString);
+                    })
+                }
+            });
+            await $('#slide-left').click(function () {
+                $('.post-style-selections').css('left', '0')
+            })
 
-    $('#slide-right').click(function (){
-        let windowSize = $(window).width();
-        console.log(windowSize*0.336);
-        $('.post-style-selections').css('left', ((965-windowSize*0.336)*-1).toString() + 'px')
-    })
+            await $('#slide-right').click(function () {
+                let windowSize = $(window).width();
+                console.log(windowSize * 0.336);
+                $('.post-style-selections').css('left', ((965 - windowSize * 0.336) * -1).toString() + 'px')
+            })
 
-    $('.prompt-name-image').click(function (){
-        $('.prompt-dropdown').css({'margin-top': '-85px', 'opacity': '1'});
-        //write cite function
-    })
+            await $('.prompt-name-image').click(function () {
+                $('.prompt-dropdown').css({'margin-top': '-85px', 'opacity': '1'});
+                //write cite function
+                document.getElementById('currently-citing').innerHTML = "Currently citing: " + $('p', this).attr('id')
+            })
 
-    $('#cancel').click(function (){
-        $('.prompt-dropdown').css({'margin-top': '-255px', 'opacity': '0'});
+            await $('#cancel').click(function () {
+                $('.prompt-dropdown').css({'margin-top': '-255px', 'opacity': '0'});
+            })
+        }
     })
-
 });
