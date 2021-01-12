bb14e932ab8d5e4abf2ad4ffb8f8026344043bbc
brandon wang <70977266+brandon-m-wang@users.noreply.github.com>
2021-01-11 23:50:42 -0600

    111

--- public/js/home.js
+++ public/js/home.js
@@ -31,6 +31,18 @@ $(document).ready(function () {
         }
     });
 
+
+    $(document).click(function (event) {
+        if (event.target.className == "comment" || $.inArray("post-the-comment", event.target.classList) !== -1) {
+            return
+        }
+        var $target = $(event.target);
+        console.log($target.parent())
+        if ($target.parent().find('.post-the-comment').css('visibility') == 'visible') {
+            $target.parent().find('.post-the-comment').css({'visibility': 'hidden', 'pointer-events': 'none'})
+        }
+    })
+
     firebase.auth().onAuthStateChanged(async function (user) {
         if (user) {
             var userID = firebase.auth().currentUser.uid;
@@ -68,13 +80,17 @@ $(document).ready(function () {
                     var liked = transformedLikes.has(userID)
                     console.log(liked)
                     var comments = doc.data().comments
-                    var numComments = doc.data().comments.length
+                    var transformedComments = new Map();
+                    Object.keys(comments).forEach(e => {
+                        transformedComments.set(String(e), comments[e]);
+                    });
+                    var numComments = transformedComments.size
                     await firebase.firestore().collection('users').doc(poster).get().then(function (doc) {
                         var posterUsername = doc.data().name
                         firebase.firestore().collection('users').doc(postee).get().then(function (doc) {
                             var posteeUsername = doc.data().name
                             if (!liked) {
-                                let htmlString = htmlToElement(`<div class="post ${theme}">
+                                let htmlString = htmlToElement(`<div class="post ${theme}" id="${postID}">
                                 <div class="post-header">
                                     <h1 style="display: none" data-value = ${postID}></h1>
                                     <a href="https://contextr.io/users/${posteeUsername}"><h3>@${posteeUsername}</h3></a>
@@ -99,12 +115,14 @@ $(document).ready(function () {
                                     </div>
                                 </div>
                                 <div class="post-comments">
-                                    <h6>Leave a comment...</h6>
+                                    <textarea class="comment" placeholder="Leave a comment..."></textarea>
+                                    <i style="pointer-events: none; visibility: hidden;" class="fas fa-paper-plane post-the-comment"></i>
                                 </div>
+                                <div class="individual-comments">test</div>
                             </div>`)
                                 $('.content-area > .container').append(htmlString)
                             } else {
-                                let htmlString = htmlToElement(`<div class="post ${theme}">
+                                let htmlString = htmlToElement(`<div class="post ${theme}" id="${postID}">
                                 <div class="post-header">
                                     <h1 style="display: none" data-value = ${postID}></h1>
                                     <a href="https://contextr.io/users/${posteeUsername}"><h3>@${posteeUsername}</h3></a>
@@ -129,13 +147,64 @@ $(document).ready(function () {
                                     </div>
                                 </div>
                                 <div class="post-comments">
-                                    <h6>Leave a comment...</h6>
+                                    <textarea class="comment" placeholder="Leave a comment..."></textarea>
+                                    <i style="pointer-events: none; visibility: hidden;" class="fas fa-paper-plane post-the-comment"></i>
                                 </div>
                             </div>`)
                                 $('.content-area > .container').append(htmlString)
                             }
                         })
                     })
+                    if (numComments > 3) {
+                        firebase.firestore().collection('posts').doc(postID).get().then(async function () {
+                                console.log("run")
+                                for (const [key, value] of Object.entries(comments)) {
+                                    let time = key
+                                    let commentUserID = value[0]
+                                    let commentString = value[1]
+                                    await firebase.firestore().collection('users').doc(commentUserID).get().then(async function (doc) {
+                                        let commentUsername = await doc.data().name
+                                        let htmlString = htmlToElement(`<div class="individual-comments" id="${time+commentUserID}">
+                                        <div class="container-individual-comments">
+                                            <a><h6>@${commentUsername}</h6></a>
+                                            <p>${commentString}</p>
+                                        </div>
+                                    </div>`)
+                                        document.getElementById(postID).appendChild(htmlString)
+                                    })
+                                    await firebase.storage().ref().child('users/' + commentUserID + '/profile').getDownloadURL().then(async function (result) {
+                                            var imgUrl = await result
+                                            let imgString = htmlToElement(`<img src=${imgUrl}/>`)
+                                            document.getElementById(time+commentUserID).prepend(imgString)
+                                    })
+                                }
+                            }
+                    } else {
+                        firebase.firestore().collection('posts').doc(postID).get().then(async function () {
+                                console.log("run")
+                                for (const [key, value] of Object.entries(comments)) {
+                                    let time = key
+                                    let commentUserID = value[0]
+                                    let commentString = value[1]
+                                    await firebase.firestore().collection('users').doc(commentUserID).get().then(async function (doc) {
+                                        let commentUsername = await doc.data().name
+                                        let htmlString = htmlToElement(`<div class="individual-comments" id="${time+commentUserID}">
+                                        <div class="container-individual-comments">
+                                            <a><h6>@${commentUsername}</h6></a>
+                                            <p>${commentString}</p>
+                                        </div>
+                                    </div>`)
+                                        document.getElementById(postID).appendChild(htmlString)
+                                    })
+                                    await firebase.storage().ref().child('users/' + commentUserID + '/profile').getDownloadURL().then(async function (result) {
+                                            var imgUrl = await result
+                                            let imgString = htmlToElement(`<img src=${imgUrl}/>`)
+                                            document.getElementById(time+commentUserID).prepend(imgString)
+                                    })
+                                }
+                            }
+                        )
+                    }
                 })
             }
             await firebase.firestore().collection("users").doc(userID).get().then(async function (doc) {
@@ -186,6 +255,32 @@ $(document).ready(function () {
                 $('body').addClass('modal-active');
             });
 
+            $('.comment').on('click', (e) => {
+                firebase.auth().onAuthStateChanged(function (user) {
+                    if (user) {
+                        $(e.target).parent().find('i').css({'visibility': 'visible', 'pointer-events': 'all'})
+                    }
+                })
+            })
+
+            $('.post-the-comment').on('click', (e) => {
+                firebase.auth().onAuthStateChanged(function (user) {
+                    if (user) {
+                        var userID = firebase.auth().currentUser.uid
+                        var postID = $(e.target).parent().parent().find('h1').attr('data-value')
+                        var commentContent = $(e.target).parent().find('.comment').val()
+                        var currTime = new Date().getTime()
+                        console.log(currTime)
+                        console.log(commentContent);
+                        firebase.firestore().collection('posts').doc(postID).set({
+                            "comments": {
+                                [currTime]: [userID, commentContent]
+                            }
+                        }, {merge: true})
+                    }
+                })
+            })
+
             $('#slide-left').click(function () {
                 $('.post-style-selections').css('left', '0')
             })
@@ -197,14 +292,25 @@ $(document).ready(function () {
             })
 
             $('.prompt-name-image').click(function () {
-                $('.prompt-dropdown').css({'margin-top': '-35px', 'margin-bottom': '130px', 'opacity': '1', 'pointer-events': 'all', 'cursor': 'pointer'});
+                $('.prompt-dropdown').css({
+                    'margin-top': '-35px',
+                    'margin-bottom': '130px',
+                    'opacity': '1',
+                    'pointer-events': 'all',
+                    'cursor': 'pointer'
+                });
                 //write cite function
                 document.getElementById('currently-citing').innerHTML = "Currently citing: " + $('p', this).attr('id')
             })
 
             $('#cancel').click(function () {
-                $('.prompt-dropdown').css({'margin-top': '-205px', 'margin-bottom': '50px', 'opacity': '0', 'pointer-events' : 'none'});
-                $('.post-style-selections > button').css({'filter' : 'brightness(100%)'});
+                $('.prompt-dropdown').css({
+                    'margin-top': '-205px',
+                    'margin-bottom': '50px',
+                    'opacity': '0',
+                    'pointer-events': 'none'
+                });
+                $('.post-style-selections > button').css({'filter': 'brightness(100%)'});
                 document.getElementById('post-text').value = '';
                 $('#post-style-selections').attr('data-value', 'null')
             })
@@ -261,7 +367,7 @@ $(document).ready(function () {
                                 postee: cited,
                                 theme: theme,
                                 text: quote,
-                                comments: [],
+                                comments: {},
                                 likes: {},
                                 dateFormatted: postDate[0],
                                 dateRaw: postDate[1]
@@ -287,10 +393,12 @@ $(document).ready(function () {
                             if (userID in doc.data().likes) {
                                 //Unlike post if liked
                                 console.log("liked already");
-                                $(e.target).css({'background' : 'linear-gradient(to bottom, rgb(136, 123, 176) 0%,' +
-                                    ' rgba(203, 157, 156, 1) 100%)',
-                                    '-webkit-text-fill-color' : 'transparent',
-                                '-webkit-background-clip': 'text'})
+                                $(e.target).css({
+                                    'background': 'linear-gradient(to bottom, rgb(136, 123, 176) 0%,' +
+                                        ' rgba(203, 157, 156, 1) 100%)',
+                                    '-webkit-text-fill-color': 'transparent',
+                                    '-webkit-background-clip': 'text'
+                                })
                                 post.set({
                                     "likes": {
                                         [userID]: firebase.firestore.FieldValue.delete()
@@ -299,8 +407,10 @@ $(document).ready(function () {
                             } else {
                                 //Like post if not liked already
                                 console.log("not liked already");
-                                $(e.target).css({'background' : 'transparent',
-                                    '-webkit-text-fill-color' : 'mediumvioletred'})
+                                $(e.target).css({
+                                    'background': 'transparent',
+                                    '-webkit-text-fill-color': 'mediumvioletred'
+                                })
                                 post.set({
                                     "likes": {
                                         [userID]: true
