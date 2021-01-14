5ec8d611a992d4a0b7f62f67bf3787bd855caa4e
zanenasrallah <71055457+zanenasrallah@users.noreply.github.com>
2021-01-14 17:43:32 -0600

    search

--- public/js/search.js
+++ public/js/search.js
@@ -1,6 +1,24 @@
+// async function findNames2() {
+//     var input = "b";
+//     let query = firebase.firestore().collection("usernames");
+//     if (input) {
+//         const end = input.replace(
+//           /.$/, c => String.fromCharCode(c.charCodeAt(0) + 1),
+//         );
+//         query = query
+//           .where(firebase.firestore.FieldPath.documentId(), '>=', input)
+//           .where(firebase.firestore.FieldPath.documentId(), '<', end);
+//     }
+//     const result = await query.get(function(doc) {
+//         console.log(doc);
+//     });
+//     // console.log(result);
+//     return result;
+// }
+
 async function findNames() {
     var names = [];
-    var searchTerm = "z";
+    var searchTerm = document.getElementById("query").value.toLowerCase();
     var numNames = 10; //Change to number that will fit on screen
     let usernames = firebase.firestore().collection('usernames');
     if(searchTerm.length != 0){
